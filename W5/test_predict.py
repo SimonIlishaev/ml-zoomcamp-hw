35a40aa9a3ac8b8169cb19ff1fd5be979c7e9f4f
Simon Ilishaev <simon.ilishaev@gmail.com>
2022-10-09 11:12:58 +0000

    Accompanying materials to W5

---
+++ W5/test_predict.py
@@ -0,0 +1,30 @@
+import requests
+# url = 'http://0.0.0.0:9696/predict'
+url = 'https://bba4rj2ipe0n8gknfndo.containers.yandexcloud.net/predict'
+customer = {
+    "gender": "female",
+    "seniorcitizen": 0,
+    "partner": "yes",
+    "dependents": "no",
+    "phoneservice": "no",
+    "multiplelines": "no_phone_service",
+    "internetservice": "dsl",
+    "onlinesecurity": "no",
+    "onlinebackup": "yes",
+    "deviceprotection": "no",
+    "techsupport": "no",
+    "streamingtv": "no",
+    "streamingmovies": "no",
+    "contract": "two_year",
+    "paperlessbilling": "yes",
+    "paymentmethod": "electronic_check",
+    "tenure": 1,
+    "monthlycharges": 29.85,
+    "totalcharges": 29.85
+}
+
+response = requests.post(url, json=customer).json()
+
+if response['churn']:
+    print(response)
+    print('sending email to', 'asdx-123d')
